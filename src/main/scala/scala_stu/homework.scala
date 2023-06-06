320da4dc478a92d79f160a555dd6782633926749
yc <2386302421@qq.com>
2023-06-06 15:30:04 +0800

    2023/6/6

--- src/main/scala/scala_stu/homework.scala
+++ src/main/scala/scala_stu/homework.scala
@@ -16,6 +16,18 @@ object homework {
       } else {
         println("猜小了")
       }
+      var map1: Map[String, Int] = Map()
+      map1+=("tom"->1)
+      val tup1 = ("1","2","3")
+      println(tup1._1)
+      def mysum(list:Int *): Unit = {
+        var s = 0;
+        for (i<-list){
+          s = s+i
+        }
+        return s
+      }
+      println(mysum(2,3,4,5,6,6,6,1))
 
 
     }
